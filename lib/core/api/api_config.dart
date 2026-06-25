9745938fdb8e09e2691377cbfd376419ae96c734
FrankZV <135568329+FrankZV@users.noreply.github.com>
2026-06-25 10:29:05 -0600

    chore: point frontend to online API

--- lib/core/api/api_config.dart
+++ lib/core/api/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   ApiConfig._();
 
-  static const String baseUrl = 'http://localhost:8000';
+  static const String baseUrl = 'https://connect-do-api.onrender.com';
 
   static const String publications = '$baseUrl/publications';
 }
